597a07c20524e8c5d5ceec6f1efa84a3b6d138b5
elluiscomun <luismiguelsantiago@outlook.es>
2024-12-25 21:20:22 -0500

    get inicial

--- js/env.js
+++ js/env.js
@@ -1,2 +1,2 @@
-//export const URL_BACKEND = 'https://spaced-repetition-q5zu.onrender.com/api/v1'
-export const URL_BACKEND = 'http://localhost:2000/api/v1'
+export const URL_BACKEND = 'https://spaced-repetition-q5zu.onrender.com/api/v1'
+//export const URL_BACKEND = 'http://localhost:2000/api/v1'
