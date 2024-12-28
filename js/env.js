f20eae20d41514e6f15afb6b048c03c8fc02322a
elluiscomun <luismiguelsantiago@outlook.es>
2024-12-28 09:55:22 -0500

    cambio servicio back

--- js/env.js
+++ js/env.js
@@ -1,2 +1,3 @@
 //export const URL_BACKEND = 'http://localhost:2000/api/v1'
-export const URL_BACKEND = 'https://spaced-repetition-ipkddw71y-elluiscomuns-projects.vercel.app/api/v1'
+//export const URL_BACKEND = 'https://spaced-repetition-ipkddw71y-elluiscomuns-projects.vercel.app/api/v1'
+export const URL_BACKEND = 'https://spaced-repetition-2-pb0ufufvu-elluiscomuns-projects.vercel.app/api/v1'
