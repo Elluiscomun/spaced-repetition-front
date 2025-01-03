57d1c2d9c27e3bcff0db6a7f4eeea29aa9353620
elluiscomun <luismiguelsantiago@outlook.es>
2025-01-02 19:21:39 -0500

    url back

--- js/env.js
+++ js/env.js
@@ -1,3 +1,2 @@
 //export const URL_BACKEND = 'http://localhost:2000/api/v1'
-//export const URL_BACKEND = 'https://spaced-repetition-ipkddw71y-elluiscomuns-projects.vercel.app/api/v1'
-export const URL_BACKEND = 'https://spaced-repetition-2-pb0ufufvu-elluiscomuns-projects.vercel.app/api/v1'
+export const URL_BACKEND = 'spaced-repetition-57jitnoqa-elluiscomuns-projects.vercel.app/api/v1'
